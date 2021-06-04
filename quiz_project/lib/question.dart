6bee5a453e9254ac19384be64bd4616ba21b084f
Pedro H. C <pedrohcm5@gmail.com>
2021-06-03 22:44:30 -0300

    updates on question.dart

--- quiz_project/lib/question.dart
+++ quiz_project/lib/question.dart
@@ -7,6 +7,14 @@ class Question extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(qText);
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.all(10),
+      child: Text(
+        qText,
+        style: TextStyle(fontSize: 28),
+        textAlign: TextAlign.center,
+      ),
+    );
   }
 }
