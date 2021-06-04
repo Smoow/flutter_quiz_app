455730d214ca0c1245178bb9b2b18e2d20de163a
Pedro H. C <pedrohcm5@gmail.com>
2021-06-03 21:39:37 -0300

    question widget

---
+++ quiz_project/lib/question.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Question extends StatelessWidget {
+  final String qText;
+
+  Question(this.qText);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(qText);
+  }
+}
